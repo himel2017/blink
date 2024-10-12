d2d3a64d1aeda9cdaee7ca028acc8be47c231f25
himel2017 <radalia03@gmail.com>
2024-10-13 01:36:04 +0600

    Remove experimental.serverActions from next.config.js to fix build issue

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+
+const nextConfig = {
+  experimental: {
+    // Remove or comment out the line below
+    // serverActions: true
+  },
+  // Any other configurations
+}
+
+module.exports = nextConfig;
